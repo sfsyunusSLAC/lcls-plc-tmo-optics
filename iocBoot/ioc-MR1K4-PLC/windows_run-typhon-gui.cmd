cf694a24d6934c14c797fdb9fe531158c6fc7f26
Jackson Sheppard <sheppard@slac.stanford.edu>
2020-02-23 16:16:41 -0800

    BLD: Building with ads-deploy

--- iocBoot/ioc-MR1K4-PLC/windows_run-typhon-gui.cmd
+++ iocBoot/ioc-MR1K4-PLC/windows_run-typhon-gui.cmd
@@ -7,4 +7,4 @@ echo on
         -v "C:\Users\sheppard\Documents\Visual Studio 2013\Projects\MR1K4\:/reg/g/pcds/epics/ioc/MR1K4" ^
 	-e DISPLAY=host.docker.internal:0.0 ^
 	-i pcdshub/ads-deploy:latest ^
-	"cd '/reg/g/pcds/epics/ioc/MR1K4/iocBoot/ioc-MR1K4-PLC' && pytmc stcmd --template-path /ads-deploy/tools/templates --template typhon_display.py --only-motor """/reg/g/pcds/epics/ioc/MR1K4/MR1K4/MR1K4.tsproj""" > /tmp/display.py && echo 'Running Typhon...' && python /tmp/display.py; sleep 1"
+	"cd '/reg/g/pcds/epics/ioc/MR1K4/iocBoot/ioc-MR1K4-PLC' && pytmc stcmd --template-path /ads-deploy/tools/templates --template typhon_display.py --only-motor """/reg/g/pcds/epics/ioc/MR1K4/MR1L0-MR2L0/MR1L0-MR2L0.tsproj""" > /tmp/display.py && echo 'Running Typhon...' && python /tmp/display.py; sleep 1"
